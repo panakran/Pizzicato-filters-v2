443bd6b1d932a3327d7d1b5de9088e6d59cb1ff4
unknown <panos_kr@hotmail.com>
2018-03-11 15:32:30 +0200

    adding dynamic filters and bindings

--- js/main-module.js
+++ js/main-module.js
@@ -1,6 +1,7 @@
 angular.module('MainModule', ['ui.knob', 'common-services', 'filterModule', 'filterAttrModule']).
         controller('MainModuleCtrl', function ($scope, fetchKnobVolumeConstants, fetchFilterConstants) {
-
+            $scope.fil = [];
+            $scope.fil2 = [];
 
             $scope.loadSong = function () {
                 $scope.songFile =
@@ -11,26 +12,33 @@ angular.module('MainModule', ['ui.knob', 'common-services', 'filterModule', 'fil
                             console.log('sound file loaded!');
                             $scope.songFile.play();
                             $scope.songFile.volume = $scope.volume / 100;
-                            $scope.fil = new $scope.filtersApplied[0].class;
-                            $scope.songFile.addEffect($scope.fil);
+                            angular.forEach($scope.filtersApplied, function (singleFilter, index) {
+                                console.log(singleFilter);
+                                var tempFilterClass = new singleFilter.class;
+                                $scope.fil2.push(tempFilterClass);
+                                $scope.songFile.addEffect($scope.fil2[index]);
+                            });
+
                         });
             };
             $scope.$watch('volume', function (newVolume) {
                 $scope.songFile.volume = newVolume / 100;
             });
             $scope.$watch('filtersApplied', function (newVolume) {
-                console.log("change", newVolume);
-                $scope.fil['gain'] = parseFloat(newVolume[0].props[0].defaults);
-//                $scope.filtersApplied[0].class. = parseFloat(newVolume);
+                angular.forEach(newVolume, function (value1, index) {
+                    angular.forEach(value1.props, function (value2) {
+                        $scope.fil2[index][value2.name] = parseFloat(value2.defaults);
+                    });
+                });
             }, true);
-            
+
             $scope.volume = 65;
             $scope.options = fetchKnobVolumeConstants;
             fetchFilterConstants.then(function (data) {
 
-                $scope.filters = data
-                $scope.filtersApplied = [$scope.filters[3]];
-            }, function (reason) {
+                $scope.filters = data;
+                $scope.filtersApplied = [$scope.filters[3], $scope.filters[1]];
+            }, function () {
 
             });
 
